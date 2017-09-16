0e1f2c502406d605236beba801178132869b3516
alfredlima <alfredolms@ic.ufal.br>
2017-09-16 10:47:44 -0300

    Cometario

--- algoritmos/trees/Fenwick1D.cpp
+++ algoritmos/trees/Fenwick1D.cpp
@@ -6,19 +6,23 @@ struct Fw
 	vector<int> ft;
 	Fw( vector<int> & v )
 	{
+		// Construção N*lg(N)
 		ft.resize(v.size()+1,0);
 		for (int i = 0; i < v.size() ; ++i)
 			update(i+1,v[i]);		
 	}
+	// lg(N)
 	void update( int k , int v ) {
 		while( k < ft.size() ) {
 			ft[k] += v;
 			k += LSB(k);
 		}
 	}
+	// lg(N)
 	int query(int r, int l){
 		return query(r) - query(l);
 	}
+	// lg(N)
 	int query(int r){
 		int sum = 0;
 		while( r ){
